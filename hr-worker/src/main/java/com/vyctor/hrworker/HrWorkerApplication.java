d6b211561174818b6a7db3dd70e6b3269af0e1dd
VyctorCosta <vyctorcosta00@gmail.com>
2023-04-07 16:51:58 -0300

    Configure Random Ports to Workers

--- hr-worker/src/main/java/com/vyctor/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/vyctor/hrworker/HrWorkerApplication.java
@@ -2,7 +2,10 @@ package com.vyctor.hrworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+//@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class HrWorkerApplication {
 
